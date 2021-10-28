a7062b69b9ff709b5d23c5643f6e2d8dc2cf3a2b
Raul Rusu <raulrusu46@gmail.com>
2021-10-28 23:27:45 +0300

    refactor: changed the name from Header to Navigation

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-white">
+    <div className="sticky top-0 z-50 bg-black text-white">
       <div className="flex items-center justify-between h-14 layout">
         <nav>
           <ul className="flex items-center justify-between space-x-4">
@@ -9,6 +9,6 @@ export default function Header() {
           </ul>
         </nav>
       </div>
-    </header>
+    </div>
   );
 }
