e21cef8a1b0af292ab7dd27c168541e8f2a709ce
Raul Rusu <raulrusu46@gmail.com>
2021-10-28 22:56:05 +0300

    add: layout components

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <a target="_blank" rel="noopener noreferrer">
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className="footer-logo" />
+        </a>
+      </footer>
+    </>
+  );
+}
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 bg-white">
+      <div className="flex items-center justify-between h-14 layout">
+        <nav>
+          <ul className="flex items-center justify-between space-x-4">
+            <li>Something</li>
+            <li>Something</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
