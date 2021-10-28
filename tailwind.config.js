767b3a51c17bb44b66b1600458d240b0ffa2efda
Raul Rusu <raulrusu46@gmail.com>
2021-10-28 22:57:52 +0300

    styles/refactor: changed the purge path from ./* -> ./src/* and added the Inter font family to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 const plugin = require("tailwindcss/plugin");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
